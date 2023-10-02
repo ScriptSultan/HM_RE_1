195671dd73e5a45b8ab975ec270d19694202a306
Denis Master <mymumhateyou26@mail.ru>
2023-10-02 17:16:22 +0300

    верно

--- main.py
+++ main.py
@@ -16,25 +16,30 @@ name_list = []
 contacts_list.pop(0)
 # pprint(parametr)
 for name in contacts_list:
-    # name_lname = name[0:3]
+
+    #сделал правильный формат имени
+
     name_lname = ' '.join(name[0:3])
     name_lname_pattern = r"([А-Яёа-я]+)\s([А-Яёа-я]+)\s([А-Яёа-я]*)(\s*)"
     new_name = r"\1, \2, \3"
     res_name = re.sub(name_lname_pattern, new_name, name_lname)
-    # name_list.append(res_name.split(', '))
     name.pop(0)
     name.pop(1)
     name.pop(2)
-    # name_list.append(res_name.split(', ') + name)
+
+    #сделал прафильный формат доб. номера
+
     number_sec_pattern = r"[\s(]*(\D+)\.\s(\d+)[\s)]*"
     new_number_sec = r" \1.\2"
     res_num_sec = re.sub(number_sec_pattern, new_number_sec, name[2])
     name[2] = res_num_sec
+
+    #сделал прафильный формат номера
+
     number_pattern = r"(\+7|8)\s*[\s(]*(\d+)[\s)]*[\s-]*(\d+)[\s-]*(\d+)[\s-]*(\d+)"
     new_number = r"+7(\2)\3-\4-\5"
     res_number = re.sub(number_pattern, new_number, name[2])
     name[2] = res_number
-    # name_list.append(name)
     name_list.append(res_name.split(', ') + name)
 
 pprint(name_list)
